a74e44056c112ddd8215b6ad553d246096d1de0c
Amirhossein Emadi <amirhossein79.e@gmail.com>
2021-09-20 05:03:53 +0430

    Work on View layer : IntroActivity

--- app/src/main/java/com/amirhosseinemadi/appstore/view/activity/IntroActivity.kt
+++ app/src/main/java/com/amirhosseinemadi/appstore/view/activity/IntroActivity.kt
@@ -3,6 +3,9 @@ package com.amirhosseinemadi.appstore.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
@@ -11,36 +14,64 @@ import com.amirhosseinemadi.appstore.databinding.ActivityIntroBinding
 import com.amirhosseinemadi.appstore.view.adapter.IntroPagerAdapter
 import com.amirhosseinemadi.appstore.view.fragment.IntroFragment
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class IntroActivity : AppCompatActivity() {
 
     private lateinit var introBinding:ActivityIntroBinding
     private lateinit var viewPager:ViewPager2
-    private lateinit var tabLayout:TabLayout
+    private lateinit var linearIndicator:LinearLayout
+    private lateinit var btnNext:AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         introBinding = DataBindingUtil.setContentView(this,R.layout.activity_intro)
         initView()
-
-        val fragment:MutableList<Fragment> = ArrayList()
-        val one:Fragment = IntroFragment()
-        val two:Fragment = IntroFragment()
-        val three:Fragment = IntroFragment()
-        fragment.add(one)
-        fragment.add(two)
-        fragment.add(three)
-        val pagerAdapter:IntroPagerAdapter = IntroPagerAdapter(supportFragmentManager,this,fragment)
-        viewPager.adapter = pagerAdapter
-        TabLayoutMediator(tabLayout,viewPager,{ tab, position -> }).attach()
-
-
+        initIndicator()
     }
+
 
     private fun initView()
     {
         viewPager = introBinding.viewPager
-        tabLayout = introBinding.tabLayout
+        linearIndicator = introBinding.linearIndicator
+        btnNext = introBinding.btnNext
+    }
+
+
+    private fun initIndicator()
+    {
+        val fragment:MutableList<Fragment> = ArrayList()
+        fragment.add(IntroFragment())
+        fragment.add(IntroFragment())
+        fragment.add(IntroFragment())
+
+        val pagerAdapter:IntroPagerAdapter = IntroPagerAdapter(supportFragmentManager,this,fragment)
+        viewPager.adapter = pagerAdapter
+
+        val views:MutableList<View> = ArrayList()
+
+        for (i:Int in 0 until fragment.size)
+        {
+            val view:View = layoutInflater.inflate(R.layout.view_pager_indicator_item,null)
+            view.findViewById<AppCompatTextView>(R.id.txt_indicator).setOnClickListener { viewPager.currentItem = i }
+            views.add(view)
+            linearIndicator.addView(view)
+        }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback()
+        {
+            override fun onPageSelected(position: Int)
+            {
+                super.onPageSelected(position)
+                for (i:Int in 0 until views.size)
+                {
+                    if (views.get(i).isSelected)
+                    {
+                        views.get(i).isSelected = false
+                    }
+                }
+                views.get(position).isSelected = true
+            }
+        })
     }
 }
